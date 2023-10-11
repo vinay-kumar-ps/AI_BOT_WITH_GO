9f84a8cfc1edb413c1c3c769c1b415d60b479b78
vinay-kumar-ps <vinaykumarps321@gmail.com>
2023-10-11 12:14:05 +0530

    changed

--- main.go
+++ main.go
@@ -7,16 +7,18 @@ import (
 	"log"
 	"os"
 
-	// "strconv"
-
 	"github.com/joho/godotenv"
+	"github.com/krognol/go-wolfram"
 	"github.com/shomali11/slacker"
 	"github.com/tidwall/gjson"
+
 	witai "github.com/wit-ai/wit-go/v2"
 )
 
-func printCommandEvents(analyticschannel <-chan *slacker.CommandEvent) {
-	for event := range analyticschannel {
+var wolframClient *wolfram.Client
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
 		fmt.Println("Command Events")
 		fmt.Println(event.Timestamp)
 		fmt.Println(event.Command)
@@ -30,16 +32,14 @@ func main() {
 	godotenv.Load(".env")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
-
 	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
-
+	wolframClient := &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")}
 	go printCommandEvents(bot.CommandEvents())
 
 	bot.Command("query for bot - <message>", &slacker.CommandDefinition{
-
 		Description: "send any question to wolfram",
-		// Examples: "who is the president of india",
-		Handler: func(botctx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+		Example:     "who is the president of india",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			query := request.Param("message")
 
 			msg, _ := client.Parse(&witai.MessageRequest{
@@ -47,12 +47,14 @@ func main() {
 			})
 			data, _ := json.MarshalIndent(msg, "", "    ")
 			rough := string(data[:])
-
 			value := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value")
-
+			answer := value.String()
+			res, err := wolframClient.GetSpokentAnswerQuery(answer, wolfram.Metric, 1000)
+			if err != nil {
+				fmt.Println("there is an error")
+			}
 			fmt.Println(value)
-			response.Reply("received")
-
+			response.Reply(res)
 		},
 	})
 
